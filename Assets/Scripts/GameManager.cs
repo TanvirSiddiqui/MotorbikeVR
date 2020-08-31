ba0247d0f8eb632f5ecf29ec17fc4a910e753dea
Md. Tanvir Siddiqui <babatanvir@hotmail.com>
2020-08-31 13:17:49 +0600

    full mechanics without graphics

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject obsticlePrefabs;
+
+    public Player player;
+    public TextMesh infoText;
+    public float gameTime;
+    public GameObject finishLine;
+    private float finalTime;
+
+
+    public float finishLinePosition = 200f;
+
+    public float spawnDistanceFromPlayer = 20f;
+    public float spawnDistanceFromObsticles = 5f;
+    public float obsticlePointer;
+    private bool isGameOver = false; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        finishLine.transform.position = new Vector3(0, 0, finishLinePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obsticlePointer < player.transform.position.z)
+        {
+            obsticlePointer += spawnDistanceFromObsticles;
+
+            GameObject obsticleObject = Instantiate(obsticlePrefabs);
+
+            obsticleObject.transform.position = new Vector3(Random.Range(-4f, 4f), 1.5f,
+                player.transform.position.z + spawnDistanceFromPlayer);
+        }
+
+        gameTime += Time.deltaTime;
+
+        if (isGameOver == false)
+        {
+            if (player.reachedFinishedLine == true)
+            {
+                isGameOver = true;
+                finalTime = gameTime;
+            }
+            infoText.text = "Time :" + Mathf.FloorToInt(gameTime);
+        }
+        else
+        {
+            infoText.text = "GAME OVER!!\nYour Time :" + Mathf.FloorToInt(finalTime);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0f;
+    public float acceleration = 0.5f;
+    public float maximumspeed = 10f;
+
+    public float distance = 5f;
+
+    private float obsticlesSlowDown = 0.5f;
+    public bool reachedFinishedLine = false;
+    private bool startAccleration = false;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("PlayerAcceleration", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Acceleration Logic
+        speed += acceleration * Time.deltaTime;
+
+        if (speed > maximumspeed)
+        {
+            speed = maximumspeed;
+        }
+
+        //Player Movement logic
+        if (startAccleration)
+        {
+            Vector3 direction = new Vector3(
+                Camera.main.transform.forward.x,
+                0,
+                Camera.main.transform.forward.z);
+            transform.position += direction.normalized * speed * Time.deltaTime;
+        }
+        if (transform.position.x < -5f) {
+            transform.position = new Vector3(-5f, transform.position.y, transform.position.z);
+        }else if (transform.position.x > 5f)
+        {
+            transform.position = new Vector3(5f, transform.position.y, transform.position.z);
+        }
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obsticle")
+        {
+            speed*=obsticlesSlowDown;
+            Debug.Log("Bari Khaise");
+        }else if (collision.gameObject.tag == "FinishLine")
+        {
+            reachedFinishedLine = true;
+            startAccleration = false;
+        }
+    }
+
+    public void PlayerAcceleration()
+    {
+        startAccleration = true;
+    }
+}
